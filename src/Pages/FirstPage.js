1322e1743fb0b9eb1392b4ec959a9b89086b7703
Akarsh Barar <barar.akarsh8@gmail.com>
2020-09-25 03:05:29 +0530

    First Page Done

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import '../css/FirstPage.css'
 import twitterlogo from '../assets/twitterlogo.svg'
-
+import SearchIcon from '@material-ui/icons/Search';
+import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 function FirstPage() {
     return (
         <div className="firstpage">
            <div className="firstpage__content">
                 <div className="firstpage__contentLeft">
+                        <div className="firstpage__contentLeftData">
+                            <div className="firstpage__contentDataLine"><SearchIcon/><h3>Follow your interests.</h3></div>
+                            <div className="firstpage__contentDataLine"><PeopleAltOutlinedIcon/><h3>Hear what people are talking about.</h3></div>
+                            <div className="firstpage__contentDataLine"><ChatBubbleOutlineIcon/> <h3>Join the conversation.</h3></div>
+                        </div>
                         <img src={twitterlogo} className="firstpage__contentLeftTwitterLogo" alt="Twiter Logo" /> 
+                        
                 </div>
                 <div className="firstpage__contentRight">
-                        <h1>RIGHT</h1>
+                       <div className="firstpage__contentRightData">
+                           <div className="firstpage__contentRightLogin">
+                               <input type="text" placeholder="Phone number email or username"/>
+                               <input type="password" placeholder="Password"/>
+                               <button>Log In</button>
+                               
+                           </div>
+                           <div className="firstpage__contentRghtForgotPassword">
+                               Forgot Password?
+                           </div>
+                           <div className="firstpage__contentRightDataa">
+                               <img src={twitterlogo} alt="Twitter Logo"/>
+                               <h1>See what’s happening in the</h1>
+                               <h1>world right now</h1>
+                               <div className="firstpage__contentDataButtons">
+                                    <h3>Join Twitter today</h3>
+                                    <button className="btn signup">Sign up</button>
+                                    <button className="btn login">Log in</button>
+                                </div>
+                           </div>
+                          
+
+                       </div>
+
                 </div>
            </div>
            <div className="firstpage__footer">
@@ -32,7 +63,7 @@ function FirstPage() {
                         <a href="#">Developers</a>
                         <a href="#">Directory</a>
                         <a href="#">Settings</a>
-                        <p>© 2020 Twitter Clone, Inc.</p>
+                        <span>© 2020 Twitter Clone</span>
                </div>
                         
                         
@@ -43,3 +74,7 @@ function FirstPage() {
 }
 
 export default FirstPage
+
+// 1000px pr login vala part gayab ho jata h
+// 800px pr vertical ho jata h
+// https://jsfiddle.net/6hpzdjnL/
